1da598dc0f170a50f96c33fc68eca232d7896d86
taesu-park <tsoo1014@gmail.com>
2019-09-09 15:38:57 +0900

    CRUD완성(받아쓰기)

---
+++ articles/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index),
+    path('new/', views.new),
+    path('index/', views.index),
+    path('create/', views.create),
+    path('<int:article_pk>/', views.detail),
+    path('<int:article_pk>/delete/', views.delete),
+    path('<int:article_pk>/edit/', views.edit),
+    path('<int:article_pk>/update/', views.update),
+
+]
---
+++ articles/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect
+from .models import Article
+
+# Create your views here.
+def index(request):
+    articles = Article.objects.order_by('-id')
+    context = {
+        'articles' : articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+def create(request):
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    article = Article.objects.create(title=title, content=content)
+    article.save()
+    context = {
+        'article':article,
+    }
+    return redirect(f'/articles/{article.pk}/')
+
+def detail(request,article_pk):
+    article = Article.objects.get(pk=article_pk)
+    context = {
+        'article' : article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+def delete(request,article_pk):
+    article = Article.objects.get(pk=article_pk)
+    article.delete()
+    return redirect('/articles/')
+
+def edit(request,article_pk):
+    article = Article.objects.get(pk=article_pk)
+    context = {
+        'article' : article,
+
+    }
+    return render(request, 'articles/edit.html/', context)
+
+def update(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    article.title = request.GET.get('title')
+    article.content = request.GET.get('content')
+    article.save()
+    return redirect(f'/articles/{article.pk}/')
