40a4f9b42c9bb4c72c9bfb399d3d17c7a60bbcf4
taesu-park <tsoo1014@gmail.com>
2019-09-16 10:45:06 +0900

    create comment(1:N) Model

--- articles/models.py
+++ articles/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 # Create your models here.
-
+# Reporter(1) - Article(N)
+# reporter - name
+class Reporter(models.Model):
+    name = models.CharField(max_length=10)
+    
 class Article(models.Model):
     title = models.CharField(max_length=10)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
 
 class Comment(models.Model):
     content = models.CharField(max_length=140)
@@ -17,3 +22,6 @@ class Comment(models.Model):
     # 2. PROTECT : 댓글이 존재하면 글 삭제 안됨.
     # 3. SET_NULL : 글이 삭제되면 NULL로 치환(NOT NULL일 경우 옵션 사용X)
     # 4. SET_DEFAULT : 디폴트 값으로 치환.
+
+# Article(1) - Comment(N)
+# comment - content
